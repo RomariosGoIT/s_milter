227c410065aa4081e66220952fba9b8833c7f8cb
RomariosGoIT <romantgt@gmail.com>
2019-03-11 12:05:58 +0200

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -66,7 +66,6 @@ $(() => {
   carouselServices();
 
   // Selectize
-
   $('select').selectize({
     create: false,
     sortField: 'text',
@@ -78,5 +77,31 @@ $(() => {
       $('.' + input).attr('readonly', true);
       $('.' + input + ', .' + wrapper).css('cursor', 'pointer');
     },
+  });
+
+  //E-mail Ajax Send
+  $('form.callback').submit(function() {
+    //Change
+    var th = $(this);
+    $.ajax({
+      type: 'POST',
+      url: '/mail.php', //Change
+      data: th.serialize(),
+    }).done(function() {
+      $(th)
+        .find('.success')
+        .addClass('active')
+        .css('display', 'flex')
+        .hide()
+        .fadeIn();
+      setTimeout(function() {
+        $(th)
+          .find('.success')
+          .removeClass('active')
+          .fadeOut();
+        th.trigger('reset');
+      }, 3000);
+    });
+    return false;
   });
 });
