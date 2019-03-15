b673e2d6c9ef0d1c29a0d400fb0fefd35a5c425f
RomariosGoIT <romantgt@gmail.com>
2019-03-15 11:43:08 +0200

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -111,6 +111,20 @@ $(function() {
     },
   });
 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > $(this).height()) {
+      $('.top').addClass('active');
+    } else {
+      $('.top').removeClass('active');
+    }
+  });
+
+  $('.top').click(function() {
+    $('html, body')
+      .stop()
+      .animate({ scrollTop: 0 }, 'slow', 'swing');
+  });
+
   //E-mail Ajax Send
   $('form.callback').submit(function() {
     //Change
@@ -136,4 +150,10 @@ $(function() {
     });
     return false;
   });
+});
+
+$(window).on('load', function() {
+  $('.preloader')
+    .delay(1000)
+    .fadeOut('slow');
 });
